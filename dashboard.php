98d684c4687b509a780e14145ca7a2d75ffe05f0
DarkShadow123456789 <95164111+DarkShadow123456789@users.noreply.github.com>
2021-12-10 09:14:20 +0530

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+
+  <title>xyz</title>
+  
+</head>
+<body>
+  <button type="submit">back</button>
+  <button type="submit">Logout</button>
+</body>
+</html>
